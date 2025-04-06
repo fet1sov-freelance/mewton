e7c1643c5b3124210aad79deafbb63e6702c1bf4
fet1sov <august9222@gmail.com>
2025-04-07 02:26:41 +0300

    fix(buffer): buffer definition fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,13 @@ import './i18next';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
+import { Buffer } from 'buffer'
+globalThis.Buffer = Buffer;
+
 const domain = location.protocol + '//' + location.host;
 const tonConnectManifestSrc = domain + "/tonconnect-manifest.json";
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
