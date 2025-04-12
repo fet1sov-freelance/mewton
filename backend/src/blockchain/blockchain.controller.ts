c7e37c1f0d3b94810110a056b0f4177a24bba0f4
fet1sov <august9222@gmail.com>
2025-04-13 01:04:43 +0300

    fix(blch): await fix

--- backend/src/blockchain/blockchain.controller.ts
+++ backend/src/blockchain/blockchain.controller.ts
@@ -11,12 +11,12 @@ export class BlockchainController {
     constructor(private blockchainService: BlockchainService) {}
 
     @Get("transaction")
-    handleTransaction(@GetUser() user: User) {
-        return this.blockchainService.checkTransaction(user.telegramId);
+    async handleTransaction(@GetUser() user: User) {
+        return await this.blockchainService.checkTransaction(user.telegramId);
     }
 
     @Post("withdraw")
-    handleWithdraw(@Body() body: { amount: number, address: string }, @GetUser() user: User) {
-        return this.blockchainService.withDrawFunds(user.telegramId, body.amount, body.address);
+    async handleWithdraw(@Body() body: { amount: number, address: string }, @GetUser() user: User) {
+        return await this.blockchainService.withDrawFunds(user.telegramId, body.amount, body.address);
     }
 }
--- backend/src/blockchain/blockchain.service.ts
+++ backend/src/blockchain/blockchain.service.ts
@@ -16,7 +16,7 @@ export class BlockchainService
         const url = "https://toncenter.com/api/v2/getTransactions";
         const params = {
             address: address,
-            limit: 2,
+            limit: 5,
             to_lt: 0,
             archival: true,
         };
