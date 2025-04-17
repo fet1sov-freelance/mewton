d07ba1effe3cf8368de0418c4050dac01cce12ed
fet1sov <august9222@gmail.com>
2025-04-18 01:38:11 +0300

    fix(withdraw): validation fix

--- frontend/src/components/Dialogs/withdraw.tsx
+++ frontend/src/components/Dialogs/withdraw.tsx
@@ -14,7 +14,7 @@ export const Withdraw = () => {
   const balance = useUserStore((state) => state.balance);
 
   const handleInputChange = (e: any) => {
-    if (e.target.value >= 0.1)
+    if (e.target.value >= 0)
     {
       setInputValue(e.target.value);
     }
